94180f9dd33bb07c2756a467053247e69016877e
imanirvansyah <imanirvansyaah@gmail.com>
2025-04-26 22:07:07 +0700

    add meta to get order list

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -4,5 +4,15 @@ import orders from "../models/orderModel";
 let data = [...orders];
 
 export const getOrders = (_: Request, res: Response) => {
-  res.json(data);
+  res.json({
+    data,
+    message: "Order data retrieved successfully",
+    status: 200,
+    meta: {
+      total: data.length,
+      totalPage: 3,
+      page: 1,
+      limit: 10,
+    }
+  });
 };
